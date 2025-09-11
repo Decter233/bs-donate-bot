d3b6c61b6183d1e1ff84b73f4b419303beecf252
Decter233 <vanyushkin20@gmail.com>
2025-09-11 11:43:38 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv()  # загружает .env
 
-BOT_TOKEN = os.getenv("7315170970:AAH2dzGe_BN1Ti9oumTlZ0po4FhL7t8GehE")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "").split(","))) if os.getenv("ADMIN_IDS") else []
 PAYMENT_TEXT = os.getenv("PAYMENT_TEXT", "Оплатите заказ {order_code} и отправьте чек.")
 QIWI_NUMBER = os.getenv("QIWI_NUMBER", "")
 YOOMONEY_WALLET = os.getenv("YOOMONEY_WALLET", "")
+DB_PATH = os.getenv("DB_PATH", "shop.sqlite3")
